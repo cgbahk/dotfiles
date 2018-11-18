6caa22bbbf7c2c5411ad62b2775642bfea55b5c5
cgbahk <cg.bahk@gmail.com>
2018-11-18 18:25:59 +0900

    [venv] Add URL version command line
    Also fix little typo

--- python_venv.sh
+++ python_venv.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 # Usage:
-#   source __/python_venv
+#   source __/python_venv.sh
+#   or
+#   bash <(curl -s https://raw.githubusercontent.com/cgbahk/dotfiles/master/python_venv.sh)
 
 venv_name="env"  # TODO: command input customize
 echo "-- Generate venv on ${PWD}/${venv_name}..."
