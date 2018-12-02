6b9263d0fbfd62af33f4f7aa03b662722ce0a14d
CG Bahk <cg.bahk@gmail.com>
2018-12-02 21:08:03 +0900

    Remove immediate tmux config refresh
    Previously it cause error when no tmux server running

--- homedir/auto_tmux.sh
+++ homedir/auto_tmux.sh
@@ -3,5 +3,6 @@
 BASEDIR=$(dirname "$0")
 # todo: backup existing .tmux.conf first
 cp $BASEDIR/.tmux.conf $HOME/.tmux.conf
-tmux source-file $HOME/.tmux.conf
 
+# for immediate refresh, run this. 
+#tmux source-file $HOME/.tmux.conf
