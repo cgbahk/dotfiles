68e420f6cbef65ef87e25d708630f20b79673510
CG Bahk <cg.bahk@gmail.com>
2018-12-13 00:59:04 +0900

    Edit default venv name to 'venv'

--- python_venv.sh
+++ python_venv.sh
@@ -4,9 +4,9 @@
 #   or
 #   curl -sL https://raw.githubusercontent.com/cgbahk/dotfiles/master/python_venv.sh > __VENV_TEMP__ && source __VENV_TEMP__ && rm __VENV_TEMP__
 
-venv_name="env"  # TODO: command input customize
+venv_name="venv"  # TODO: command input customize
 echo "-- Generate venv on ${PWD}/${venv_name}..."
-python3.6 -m venv ${venv_name} # by default python 3.6
+python3.6 -m venv ${venv_name} # by default python 3.6 TODO: make this reliable
 
 echo "-- Activate venv..."
 source ${venv_name}/bin/activate
