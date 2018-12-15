a8737d89e30f6424a5f16a0c880727d666eb104f
CG Bahk <cg.bahk@gmail.com>
2018-12-15 13:45:29 +0900

    Add comments on 'auto_homedir.sh'

--- auto_homedir.sh
+++ auto_homedir.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
 
+# get directories
 basedir=$(dirname "$0")
 homedir="$basedir/homedir"
 
+# run all autos in homedir
 autos=$(ls $homedir | grep "auto*")
 for auto in $autos; do
     $homedir/$auto
