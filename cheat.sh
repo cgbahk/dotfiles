89e1c87279552c0529f0569efeb982e64c152063
CG Bahk <cg.bahk@gmail.com>
2018-12-22 23:26:10 +0900

    Move docker remove command to alias 'dockerrmi'

--- cheat.sh
+++ cheat.sh
@@ -1,6 +1,3 @@
 # check linux distribution
 cat /etc/os-release
 
-# docker rmi <none>s
-docker rmi $(docker images -f "dangling=true" -q)
-
--- windows/Microsoft.PowerShell_profile.ps1
+++ windows/Microsoft.PowerShell_profile.ps1
@@ -1,6 +1,7 @@
 # docker alias
 function dockerrun { docker run --rm -it $args }
 function dockerrunpwd { dockerrun -v ${PWD}:/project -w /project $args }
+function dockerrmi { docker rmi $(docker images -f "dangling=true" -q) }
 
 # git alias
 function github { git clone https://github.com/$args }
