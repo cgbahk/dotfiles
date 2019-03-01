cfff80d7cba5615a213db479753cbc5a88228f9e
CG Bahk <cg.bahk@gmail.com>
2019-03-01 11:35:36 +0900

    Introduce vim custom configuration
    Add default custom setting for shiftwidth, gitgutter plugin and
    colorscheme.

--- homedir/auto_amix_vim.sh
+++ homedir/auto_amix_vim.sh
@@ -3,3 +3,6 @@
 git clone --depth=1 https://github.com/amix/vimrc.git ~/.vim_runtime
 sh ~/.vim_runtime/install_awesome_vimrc.sh
 
+homedir=$(dirname "$0")
+config=my_configs.vim
+cp $homedir/$config ~/.vim_runtime/$config
---
+++ homedir/my_configs.vim
@@ -0,0 +1,3 @@
+set shiftwidth=2
+let g:gitgutter_enabled=1 " TODO Not sure this is applied
+set t_Co=256
