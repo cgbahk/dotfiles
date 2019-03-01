fe7e72422c2e7dd3540d37d697c03d431ae58672
CG Bahk <cg.bahk@gmail.com>
2019-03-01 11:46:29 +0900

    Add TODO comments for homedir automation

--- auto_homedir.sh
+++ auto_homedir.sh
@@ -5,6 +5,8 @@ basedir=$(dirname "$0")
 homedir="$basedir/homedir"
 
 # run all autos in homedir
+# TODO make users to choose what they want to apply, not all
+# TODO make it more verbose
 autos=$(ls $homedir | grep "auto*")
 for auto in $autos; do
     $homedir/$auto
--- homedir/auto_amix_vim.sh
+++ homedir/auto_amix_vim.sh
@@ -1,5 +1,6 @@
 #! /bin/bash
 
+# TODO check whether installed already
 git clone --depth=1 https://github.com/amix/vimrc.git ~/.vim_runtime
 sh ~/.vim_runtime/install_awesome_vimrc.sh
 
