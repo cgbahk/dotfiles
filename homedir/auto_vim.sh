19d0bf350fc85734c4e54bd4ab47f2ee299b56ff
CG Bahk <cg.bahk@gmail.com>
2019-03-13 19:43:08 +0900

    Delete custom configuration for vim
    vim configuration would be mananged by cgbahk/vimrc

--- homedir/auto_vim.sh
+++ homedir/auto_vim.sh
@@ -3,8 +3,3 @@
 # TODO check whether installed already
 git clone --depth=1 https://github.com/amix/vimrc.git ~/.vim_runtime
 sh ~/.vim_runtime/install_awesome_vimrc.sh
-
-# apply custom config
-homedir=$(dirname "$0")
-config=my_configs.vim
-cp $homedir/$config ~/.vim_runtime/$config
--- homedir/my_configs.vim
+++
@@ -1,3 +0,0 @@
-set shiftwidth=2
-let g:gitgutter_enabled=1 " TODO Not sure this is applied
-set t_Co=256
