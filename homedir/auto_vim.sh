e75d4e157b776dd62bcff11352cbbb631a77d6f6
CG Bahk <cg.bahk@gmail.com>
2019-03-13 19:44:04 +0900

    Use cgbahk/vimrc for vim configuration
    From now on, use (forked) cgbahk/vimrc. Treat all vim-related issue in
    there.

--- homedir/auto_vim.sh
+++ homedir/auto_vim.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
 
 # TODO check whether installed already
-git clone --depth=1 https://github.com/amix/vimrc.git ~/.vim_runtime
+git clone --depth=1 https://github.com/cgbahk/vimrc.git ~/.vim_runtime
 sh ~/.vim_runtime/install_awesome_vimrc.sh
