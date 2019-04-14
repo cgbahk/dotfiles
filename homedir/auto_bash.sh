18c76ae0e9ad459694df17996376cc95b113bcea
CG Bahk <cg.bahk@gmail.com>
2019-04-15 00:39:03 +0900

    Checking '.bashrc' actually not doing anything but echo
    Also minor TODO change

--- homedir/auto_bash.sh
+++ homedir/auto_bash.sh
@@ -12,18 +12,16 @@ echo "-- copy '$fname' to $HOME..."
 BASEDIR=$(dirname "$0")
 cp $BASEDIR/$fname $HOME/$fname
 
-# todo: check '.bashrc' or .bash_profile exists
+# TODO: check '.bashrc' or .bash_profile exists
 echo "-- check '.bashrc' whether it refers '$fname'..."
 ref=$(grep $fname $HOME/.bashrc)
 if [ -n $"ref" ]; then
-    echo "Warning: '$fname' refered in '$HOME/.bashrc'. Check out manually to add in '$fname'"
-    exit 255
+  echo "-- '$fname' refered in '$HOME/.bashrc'. '$fname' maybe sourced already."
+else
+  echo "-- Please edit '.bashrc' or '.bash_profile' to include '$fname' manually"
 fi
 
-echo "-- Add '.bashrc' to include '$fname'..."
-echo "if [ -f ~/.bash_aliases  ]; then; . ~/.bash_aliases; fi" >> $HOME/.bashrc
-
-# todo: actually this is not working immediately, but applied to next session
+# TODO: actually this is not working immediately, but applied to next session
 echo "-- register aliases..."
 source $HOME/$fname
 
