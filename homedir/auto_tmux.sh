2a26e4f150b735c11ea8c8e6469b18c745d560ae
CG Bahk <cg.bahk@gmail.com>
2019-04-28 18:21:19 +0900

    Use my fork for tmux config

--- homedir/auto_tmux.sh
+++ homedir/auto_tmux.sh
@@ -1,8 +1,21 @@
 #! /bin/bash
 
-# github@tony/tmux-config
-git clone --recursive https://github.com/tony/tmux-config.git ~/.tmux
-if [ -f ~/.tmux.conf ]; then
-    mv ~/.tmux.conf ~/.tmux.conf.backup
+TMUX_CONF_DIR=~/.tmux
+TMUX_CONF_REPO="https://github.com/cgbahk/tmux-config"
+mkdir -p ${TMUX_CONF_DIR}
+cd ${TMUX_CONF_DIR}
+# TODO remove error message for some case,as it is not actually error for this
+ORIGIN_URL=$(git remote get-url origin)
+
+if [ $? -ne 0 ]; then
+  git clone ${TMUX_CONF_REPO} ${TMUX_CONF_DIR}
+elif echo ${ORIGIN_URL} | grep -q "cgbahk"; then
+  git pull
+  git checkout downstream
+else
+  git remote set-url origin ${TMUX_CONF_REPO}
+  git pull
+  git checkout downstream
 fi
+
 ln -s ~/.tmux/.tmux.conf ~/.tmux.conf
