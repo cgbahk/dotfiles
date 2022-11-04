e5f3a1debd1773e1e675ab32d46b8b8b0ac33a45
Cheongyo Bahk <cg.bahk@gmail.com>
2022-11-04 15:08:50 +0900

    [pudb] Use existing default stringifier

--- homedir/.config/pudb/stringifier.py
+++ homedir/.config/pudb/stringifier.py
@@ -1,34 +1,4 @@
-from collections.abc import Sized
-
-
-def mock_default_stringifier(value):
-    """
-    Simpler version of default stringifier
-    Reference: https://github.com/inducer/pudb/blob/dab821fe50/pudb/var_view.py#L399
-
-    TODO Can we use default stringifier?
-    https://github.com/inducer/pudb/discussions/520
-    """
-    BASIC_TYPES = (
-        type(None),
-        int,
-        str,
-        float,
-        complex,
-    )
-
-    if isinstance(value, BASIC_TYPES):
-        return repr(value)
-    elif isinstance(value, Sized):
-        try:
-            # Example: numpy arrays with shape == () raise on len()
-            obj_len = len(value)
-        except TypeError:
-            pass
-        else:
-            return f"{type(value).__name__} ({obj_len})"
-
-    return str(type(value).__name__)
+from pudb.var_view import default_stringifier
 
 
 def get_fullname(obj):
@@ -45,4 +15,4 @@ def pudb_stringifier(obj):
 
         return f"<torch.Tensor> Shape {list(obj.shape)}"
 
-    return mock_default_stringifier(obj)
+    return default_stringifier(obj)
